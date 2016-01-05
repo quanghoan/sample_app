21ea5f4fba99a5419c12d2acc5c782a1d290e0c5
hoan <quanghoan1992@gmail.com>
2016-01-05 14:56:43 +0700

    change some  css

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
-	before_action :correct_user, only: :destroy
 
 	def create
 		@micropost = Micropost.find(params[:micropost_id])		
@@ -17,6 +16,7 @@ class CommentsController < ApplicationController
 
 	def destroy
 		@micropost = Micropost.find(params[:micropost_id])	
+		@comment = @micropost.comments.find(params[:id])
 		@comment.destroy
 		respond_to do |format|
 			format.html do 
