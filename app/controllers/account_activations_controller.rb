98c5cdc9ac37107c2684771445cb3735d7897741
hoan <quanghoan1992@gmail.com>
2016-01-06 15:53:30 +0700

    fix mailer

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,7 +2,6 @@ class AccountActivationsController < ApplicationController
 
   def edit
     user = User.find_by(email: params[:email])
-    
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
