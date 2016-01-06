e4cc8ce7a90e5bd88ac9c438f4ad2366e777e300
hoan <quanghoan1992@gmail.com>
2016-01-06 14:48:30 +0700

    adf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,8 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.perform_deliveries = true
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
