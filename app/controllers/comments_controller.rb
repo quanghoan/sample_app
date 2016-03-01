7ffebc2a5ce1c988ff78070a2962de45e5213f34
quanghoan <quanghoan1992@gmail.com>
2016-03-01 22:35:55 +0700

    add meta tag

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   # before_action :logged_in_user, only: [:create, :destroy]
-  # before_action :logged_in_user
+  before_action :logged_in_user
 	def create
 		@micropost = Micropost.find(params[:micropost_id])		
 		@comment = @micropost.comments.build(comment_params)
